f9414d687b1813aa88ba419fac10411116e04671
lyyliyongyi <lyyliyongyi@126.com>
2024-07-02 09:19:22 +0000

    修改标题

--- fmNewsChat.py
+++ fmNewsChat.py
@@ -31,20 +31,11 @@ def on_chat_change():
 
 in_expander = False
 show_history = False
-# 左侧工具栏
-with st.sidebar:
-    st.subheader('凤凰FM交互式资讯知识助手')
-    chat_name = st.selectbox("会话名称:", ["默认", "会话1"],
-                             key="chat_name",
-                             on_change=on_chat_change)
-    chat_box.use_chat_name(chat_name)
-    streaming = st.checkbox('流式输出', key="streaming", value=True)
-    chat_box.context_from_session(
-        exclude=["chat_name"])  # save widget values to chat context
-
-    st.divider()
-
-    btns = st.container()
+chat_name = "默认"
+chat_box.use_chat_name(chat_name)
+streaming = True
+chat_box.context_from_session(exclude=["chat_name"])
+btns = st.container()
 
 chat_box.init_session()
 chat_box.output_messages()
@@ -67,38 +58,40 @@ feedback_kwargs = {
     "feedback_type": "thumbs",
     "optional_text_label": "wellcome to feedback",
 }
+# 创建一个columns容器，将输入框和按钮放在同一行
+col1, col2 = st.columns([9, 1])
+with col1:
+    if query := st.chat_input('输入您要听的资讯'):
+        chat_box.user_say(query)
+        chat_box.ai_say([
+            Markdown("思考中...",
+                     in_expander=in_expander,
+                     expanded=True,
+                     title="answer")
+        ])
+        if streaming:
+            # generator = llm.chat_stream(query)
+            os.environ[
+                "APPBUILDER_TOKEN"] = 'bce-v3/ALTAK-JDdbJTm8UQvC6FcVmvYf3/ad840bbae22aa62ebd916d5f81d75d580d5ed00e'
+            app_id = '1abd2214-1440-46f8-9968-78f6115bf5e2'  # 已发布AppBuilder应用的ID
+            # 初始化智能体
+            client = appbuilder.AppBuilderClient(app_id)
+            # 创建会话
+            conversation_id = client.create_conversation()
+            message = client.run(conversation_id, query, stream=True)
 
-if query := st.chat_input('输入您要听的资讯'):
-    chat_box.user_say(query)
-    chat_box.ai_say([
-        Markdown("思考中...",
-                 in_expander=in_expander,
-                 expanded=True,
-                 title="answer")
-    ])
-    if streaming:
-        # generator = llm.chat_stream(query)
-        os.environ[
-            "APPBUILDER_TOKEN"] = 'bce-v3/ALTAK-JDdbJTm8UQvC6FcVmvYf3/ad840bbae22aa62ebd916d5f81d75d580d5ed00e'
-        app_id = '1abd2214-1440-46f8-9968-78f6115bf5e2'  # 已发布AppBuilder应用的ID
-        # 初始化智能体
-        client = appbuilder.AppBuilderClient(app_id)
-        # 创建会话
-        conversation_id = client.create_conversation()
-        message = client.run(conversation_id, query, stream=True)
-
-        text = ""
-        for content in message.content:
-            text += content.answer
-            chat_box.update_msg(text, element_index=0, streaming=True)
-            for event in content.events:
-                content_type = event.content_type
-                detail = event.detail
-                print(content_type)
-
-if btns.button("清空记录"):
-    chat_box.init_session(clear=True)
-    st.experimental_rerun()
+            text = ""
+            for content in message.content:
+                text += content.answer
+                chat_box.update_msg(text, element_index=0, streaming=True)
+                for event in content.events:
+                    content_type = event.content_type
+                    detail = event.detail
+                    print(content_type)
+with col2:
+    if btns.button("清空记录"):
+        chat_box.init_session(clear=True)
+        st.experimental_rerun()
 
 if show_history:
     st.write(st.session_state)
